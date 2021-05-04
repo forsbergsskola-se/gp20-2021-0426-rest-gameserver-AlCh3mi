9f0c144a1be23d2bbdd5dbc0313282b6f45ee7be
Michael Van Der Poll <michaelvdp@gmail.com>
2021-05-04 12:18:28 +0200

    Got response from API working with https request

--- GitHubExplorer/Program.cs
+++ GitHubExplorer/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
-using System.Net.Http;
+using System.IO;
+using System.Net;
+using System.Threading.Tasks;
 
 namespace GitHubExplorer
 {
@@ -8,13 +10,36 @@ namespace GitHubExplorer
         //GitHub Personal Access Token
         //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
         
-        //Apex API
+        //Apex Legends API Token
         //X8MmHiCTDGB3tCgZe0iv
         
-        static void Main(string[] args)
+        static void Main(string[] args) {
+            var apexApi = new ApexLegendsApiInteraction();
+            var response = apexApi.Request("https://api.mozambiquehe.re/maprotation?auth=X8MmHiCTDGB3tCgZe0iv");
+            Console.Write(response.Result);
+            Console.ReadKey();
+        }
+    }
+
+    public class ApexLegendsApiInteraction {
+        
+        //Player Info Request
+        //https://api.mozambiquehe.re/bridge?platform=PC&player=DeadwoodZa&auth=X8MmHiCTDGB3tCgZe0iv
+        
+        //Current Map Info
+        //https://api.mozambiquehe.re/maprotation?auth=X8MmHiCTDGB3tCgZe0iv
+        
+        
+        public async Task<string> Request(string url)
         {
-            HttpClient httpClient = new HttpClient();
-            
+            ServicePointManager.ServerCertificateValidationCallback = 
+                (a, b, c, d) => true;
+
+            var req = (HttpWebRequest)WebRequest.Create(url);
+            var resp = (HttpWebResponse) await req.GetResponseAsync();
+            using var sr = new StreamReader(resp.GetResponseStream());
+            var results = await sr.ReadToEndAsync();
+            return results;
         }
     }
 }
