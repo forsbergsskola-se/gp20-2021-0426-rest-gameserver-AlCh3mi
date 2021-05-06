bd6c3633487d4a2bb2e675f49d088c1457e6311c
Michael Van Der Poll <michaelvdp@gmail.com>
2021-05-06 10:18:41 +0200

    Adds NewtonSoft Json Serialization package

--- GitHubExplorer/ApexLegends/Auth.cs
+++ GitHubExplorer/ApexLegends/Auth.cs
@@ -1,4 +1,5 @@
-﻿namespace GitHubExplorer.ApexLegends {
+﻿using Newtonsoft.Json;
+namespace GitHubExplorer.ApexLegends {
     public struct Auth {
         public string Token { get; private set; }
     }
